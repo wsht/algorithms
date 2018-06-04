6bce2ce9f626b63900423bb74b039a179ad12967
wsht <952587282@qq.com>
2018-06-04 13:01:38 +0800

    preorder and inorder without recursion

--- tree/traversal/traversal.go
+++ tree/traversal/traversal.go
@@ -3,6 +3,39 @@ package main
 import "fmt"
 
 /**
+DLR
+前序遍历
+先访问根节点， 再前序访问左节点，最后再前序访问右节点  根-左-右
+*/
+
+func PreOrderTraverse(t *BitTree) {
+	if t != nil {
+		println(t.Data)
+		PreOrderTraverse(t.LChild)
+		PreOrderTraverse(t.RChild)
+	}
+}
+
+func PreOrderTraverseWithoutRecursion(t *BitTree) {
+	stack := []*BitTree{}
+	tmp := t
+	//将左子树压入栈后，出栈，再获取右子树，然后再压入右子树的左子树
+	for tmp != nil || len(stack) > 0 {
+		for tmp != nil {
+			stack = append(stack, tmp)
+			fmt.Println(tmp.Data)
+			tmp = tmp.LChild
+		}
+
+		if len(stack) != 0 {
+			tmp = stack[len(stack)-1:][0]
+			stack = stack[:len(stack)-1]
+			tmp = tmp.RChild
+		}
+	}
+}
+
+/**
 LDR
 中序访问
 先中序访问左子树，然后访问根节点，再中序访问右子树， 即左-中-右
@@ -15,17 +48,28 @@ func InOrderTraverse(t *BitTree) {
 	}
 }
 
-/**
-DLR
-前序遍历
-先访问根节点， 再前序访问左节点，最后再前序访问右节点  根-左-右
-*/
+func InorderTraverseWithoutRecursion(t *BitTree) {
+	stack := []*BitTree{}
+	tmp := t
 
-func PreOrderTraverse(t *BitTree) {
-	if t != nil {
-		println(t.Data)
-		PreOrderTraverse(t.LChild)
-		PreOrderTraverse(t.RChild)
+	if tmp == nil {
+		fmt.Println("the tree is null ")
+
+	} else {
+		for tmp != nil || len(stack) != 0 {
+			for tmp != nil {
+				stack = append(stack, tmp)
+				tmp = tmp.LChild
+			}
+			// fmt.Println("stack len", len(stack))
+			if len(stack) != 0 {
+				tmp = stack[len(stack)-1:][0]
+				// fmt.Println(tmp)
+				fmt.Println(tmp.Data)
+				stack = stack[:len(stack)-1]
+				tmp = tmp.RChild
+			}
+		}
 	}
 }
 
--- tree/traversal/tree_test.go
+++ tree/traversal/tree_test.go
@@ -19,10 +19,18 @@ func TestCreateTree(x *testing.T) {
 	// x.Log(t)
 	fmt.Println("pre order traverse")
 	PreOrderTraverse(t)
+	fmt.Println("pre order traverse without recursion")
+	PreOrderTraverseWithoutRecursion(t)
 
 	fmt.Println("in order Traverse")
 	InOrderTraverse(t)
+	fmt.Println("in order traverse without recursion")
+	InorderTraverseWithoutRecursion(t)
 
 	fmt.Println("poster order traverse")
 	PostOrderTraverse(t)
+}
+
+func TestPreOrderWithoutRecursion(testT *testing.T) {
+	// t :=
 }
